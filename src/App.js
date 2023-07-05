038ebf571a1c63c8bd4802c86209cd45a473dac6
olliechaffey <olliechaff@gmail.com>
2023-07-05 20:35:12 +0100

    Add custom font files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+// import ReactDOM from 'react-dom/client';
+import './index.css';
+// import reportWebVitals from './reportWebVitals';
+import Navbar from './Navbar.js';
+import SignUp from './signupSection.js';
+import AppDesc from './appDescSection.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Navbar />
+      <div className="main-content">
+    <SignUp />
+    <AppDesc />
+  </div>
     </div>
   );
 }
